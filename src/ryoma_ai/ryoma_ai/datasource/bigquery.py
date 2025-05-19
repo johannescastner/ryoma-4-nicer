9d148577ad54bb882b00b7da400505cb7cf0ccb2
Johannes Castner <138595172+johannescastner@users.noreply.github.com>
2025-05-19 16:30:53 +0100

    Update bigquery.py
    ok now we can use the Dataplex catalog instead of the Amundsen for both, publishing and extraction ...using still the same basic interface as before

--- src/ryoma_ai/ryoma_ai/datasource/bigquery.py
+++ src/ryoma_ai/ryoma_ai/datasource/bigquery.py
@@ -1,27 +1,43 @@
+python
 import logging
-from typing import Optional
+from typing import Optional, Type, Any
 
 import ibis
-from databuilder.job.job import DefaultJob
-from databuilder.loader.base_loader import Loader
-from databuilder.task.task import DefaultTask
 from ibis import BaseBackend
-from pyhocon import ConfigFactory
 from ryoma_ai.datasource.base import SqlDataSource
 
+# Amundsen imports for metadata pipeline
+from databuilder.loader.base_loader import Loader
+from databuilder.task.task import DefaultTask
+from databuilder.job.job import DefaultJob
+from databuilder.publisher.neo4j_csv_publisher import Neo4jCsvPublisher
+
+# Our custom Dataplex extractor & publisher
+from ryoma_ai.datasource.dataplex import DataplexMetadataExtractor, DataplexPublisher
 
 class BigQueryDataSource(SqlDataSource):
     def __init__(
         self,
         project_id: str,
         dataset_id: Optional[str] = None,
-        credentials: Optional[str] = None,
+        credentials: Optional[Any] = None,
+        *,
+        metadata_extractor_cls: Type = DataplexMetadataExtractor,
+        metadata_publisher_cls: Type = DataplexPublisher,
     ):
-        # Tell the SqlDataSource base which 'database' (i.e. dataset) to use.
+        """
+        A BigQuery data source that by default crawls metadata via Google Dataplex,
+        but can fall back to Amundsen BigQuery extractor + Neo4j publisher if desired.
+        """
+        # Tell the SqlDataSource base which 'database' (dataset) to use
         super().__init__(database=dataset_id)
-        self.project_id  = project_id
-        self.dataset_id  = dataset_id
+        self.project_id = project_id
+        self.dataset_id = dataset_id
         self.credentials = credentials
+
+        # Pluggable extractor & publisher
+        self._extractor_cls = metadata_extractor_cls
+        self._publisher_cls = metadata_publisher_cls
 
     def _connect(self, **kwargs) -> BaseBackend:
         connect_args: dict[str, Any] = {"project_id": self.project_id, **kwargs}
@@ -34,34 +50,43 @@ class BigQueryDataSource(SqlDataSource):
         return ibis.bigquery.connect(**connect_args)
 
     def crawl_catalogs(self, loader: Loader, where_clause_suffix: Optional[str] = ""):
-        from databuilder.extractor.bigquery_metadata_extractor import (
-            BigQueryMetadataExtractor,
+        """
+        Dynamically discover all datasets/tables/columns by:
+          1) instantiating the configured metadata extractor
+          2) instantiating the configured metadata publisher
+          3) running the Amundsen-style DefaultJob pipeline
+        """
+        logging.info(
+            "Crawling data catalog from BigQuery using %s",
+            self._extractor_cls.__name__,
         )
 
-        logging.info("Crawling data catalog from Bigquery")
-        job_config = ConfigFactory.from_dict(
-            {
-                "extractor.bigquery_table_metadata.{}".format(
-                    BigQueryMetadataExtractor.PROJECT_ID_KEY
-                )
-            }
-        )
-        job = DefaultJob(
-            conf=job_config,
-            task=DefaultTask(extractor=BigQueryMetadataExtractor(), loader=loader),
+        # 1) build extractor
+        extractor = self._extractor_cls(
+            project_id=self.project_id,
+            credentials=self.credentials,
         )
 
+        # 2) build publisher
+        publisher = self._publisher_cls()
+
+        # 3) launch the standard Amundsen load pipeline
+        task = DefaultTask(extractor=extractor, loader=loader)
+        job = DefaultJob(conf={}, task=task, publisher=publisher)
         job.launch()
 
-    def get_query_plan(self, query: str):                 # noqa: N802
+    def get_query_plan(self, query: str):  # noqa: N802
         """
-        BigQuery supports `EXPLAIN`.  We return an ibis Table so Ryoma
-        can pretty-print it or inspect cost estimates.
+        BigQuery supports EXPLAIN; return ibis Table for profiling.
         """
         conn = self.connect()
         return conn.sql(f"EXPLAIN {query}")
 
+
 class BigqueryDataSource(BigQueryDataSource):
+    """
+    Deprecated alias for backwards compatibility.
+    """
     def __init__(self, *args, **kwargs):
         import warnings
         warnings.warn(
