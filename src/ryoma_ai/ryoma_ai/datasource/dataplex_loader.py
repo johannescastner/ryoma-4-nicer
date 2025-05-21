7084faf2fed16cd7d75d6f4e5f3036a59b2be4ee
Johannes Castner <138595172+johannescastner@users.noreply.github.com>
2025-05-21 21:49:11 +0100

    next error debugging ...this time a missing import for TableMetadata

--- src/ryoma_ai/ryoma_ai/datasource/dataplex_loader.py
+++ src/ryoma_ai/ryoma_ai/datasource/dataplex_loader.py
@@ -1,7 +1,7 @@
 # src/ryoma_ai/ryoma_ai/datasource/dataplex_loader.py
 import logging
 from typing import Iterator, Union, List
-
+from databuilder.models.table_metadata import TableMetadata
 from databuilder.loader.base_loader import Loader
 from pyhocon import ConfigTree
 
